05ac3350b391b8306a308417f28b3171665f4abf
aoken7 <cycloaoken@gmail.com>
2023-05-19 16:23:10 +0900

    共通処理の切り出し

--- numgo/matrix.go
+++ numgo/matrix.go
@@ -1,7 +1,6 @@
 package numgo
 
 import (
-	"math"
 	"math/rand"
 	"time"
 )
@@ -29,17 +28,17 @@ func NewMx(a [][]float32) Mx {
 	return *m
 }
 
-func (m *Mx) shape() []int {
+func (m *Mx) Shape() []int {
 	s := make([]int, 2)
 	s[0] = len(m.Vec[0]) // 行数
 	s[1] = len(m.Vec)    // 列数
 	return s
 }
 
-// (line, row)の大きさでmxをランダムに初期化
+// (line, row) > 0 の大きさでmxをランダムに初期化
 func Randn(l, r int) Mx {
 	seed := time.Now().UnixNano()
-	rand.Seed(seed)
+	rd := rand.New(rand.NewSource(seed))
 
 	m := make([][]float32, l)
 	for i := 0; i < l; i++ {
@@ -48,43 +47,37 @@ func Randn(l, r int) Mx {
 
 	for i := 0; i < l; i++ {
 		for j := 0; j < r; j++ {
-			m[i][j] = rand.Float32() + 0.5
+			m[i][j] = rd.Float32() + 0.5
 		}
 	}
 
 	return NewMx(m)
 }
 
-func Exp(a Mx) Mx {
-	aShape := a.shape()
+// wの大きさが(1x1)の時は行列とスカラの和になる
+func Add(a Mx, w Mx) Mx {
+	aShape := a.Shape()
 	b := make([][]float32, aShape[1])
 
 	for i := 0; i < aShape[1]; i++ {
 		b[i] = make([]float32, aShape[0])
+
 		for j := 0; j < aShape[0]; j++ {
-			b[i][j] = float32(math.Exp(float64(a.Vec[i][j])))
+			if len(w.Vec[0]) == 1 {
+				b[i][j] = a.Vec[i][j] + w.Vec[0][0]
+			} else {
+				b[i][j] = a.Vec[i][j] + w.Vec[0][j]
+			}
 		}
 	}
 
 	return NewMx(b)
 }
 
-func AddScl(a Mx, scalar float32) Mx {
-	aShape := a.shape()
-	b := make([][]float32, aShape[1])
-
-	for i := 0; i < aShape[1]; i++ {
-		b[i] = make([]float32, aShape[0])
-		for j := 0; j < aShape[0]; j++ {
-			b[i][j] = a.Vec[i][j] + scalar
-		}
-	}
-	return NewMx(b)
-}
-
+// 行列積を計算する。aの行とbの列の大きさを合わせる必要あり。
 func Dot(a, b Mx) Mx {
-	a_shape := a.shape()
-	b_shape := b.shape()
+	a_shape := a.Shape()
+	b_shape := b.Shape()
 
 	if a_shape[0] != b_shape[1] {
 		panic("Matrix shape does not match.")
@@ -93,9 +86,6 @@ func Dot(a, b Mx) Mx {
 	c := make([][]float32, a_shape[1])
 	for i := 0; i < a_shape[1]; i++ {
 		c[i] = make([]float32, b_shape[0])
-	}
-
-	for i := 0; i < a_shape[1]; i++ {
 		for j := 0; j < b_shape[0]; j++ {
 			for k := 0; k < a_shape[0]; k++ {
 				c[i][j] += a.Vec[i][k] * b.Vec[k][j]
--- numgo/matrix_test.go
+++ numgo/matrix_test.go
@@ -16,7 +16,22 @@ func TestDot(t *testing.T) {
 		want Mx
 	}{
 		{
-			name: "2*2の行列積",
+			name: "(1x3)*(3x1)の行列積",
+			args: args{
+				a: NewMx([][]float32{{1, 2, 3}}),
+				b: NewMx([][]float32{{4}, {5}, {6}}),
+			},
+			/*
+				>>> import numpy as np
+				>>> a = np.array([1,2,3])
+				>>> b = np.array([4,5,6])
+				>>> np.dot(a,b)
+				32
+			*/
+			want: NewMx([][]float32{{32}}),
+		},
+		{
+			name: "(2x2)*(2x2)の行列積",
 			args: args{
 				a: NewMx([][]float32{{1, 2}, {3, 4}}),
 				b: NewMx([][]float32{{1, 2}, {3, 4}}),
@@ -32,7 +47,7 @@ func TestDot(t *testing.T) {
 			want: NewMx([][]float32{{7, 10}, {15, 22}}),
 		},
 		{
-			name: "2*3の行列積",
+			name: "(2x2)*(2x3)の行列積",
 			args: args{
 				a: NewMx([][]float32{{1, 2}, {3, 4}}),
 				b: NewMx([][]float32{{1, 2, 3}, {4, 5, 6}}),
@@ -71,7 +86,7 @@ func TestRandn(t *testing.T) {
 		{
 			name: "動作確認",
 			args: args{2, 3},
-			want: NewMx([][]float32{}),
+			want: NewMx([][]float32{{1}}),
 		},
 	}
 	for _, tt := range tests {
@@ -80,6 +95,95 @@ func TestRandn(t *testing.T) {
 				fmt.Println(got)
 				t.Errorf("Randn() = %v, want %v", got, tt.want)
 			} */
+		})
+	}
+}
+
+func TestMx_Exp(t *testing.T) {
+	type fields struct {
+		Vec [][]float32
+		T   [][]float32
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   Mx
+	}{
+		// TODO: Add test cases.
+		{
+			name: "(1x3)の行列のExp",
+			fields: fields{
+				Vec: [][]float32{{1}, {2}, {3}},
+			},
+			/*
+				>>> a = np.array([[1],[2],[3]])
+				>>> np.exp(a)
+				array([[ 2.71828183],
+				       [ 7.3890561 ],
+				       [20.08553692]])
+			*/
+			want: NewMx([][]float32{{2.71828183}, {7.3890561}, {20.08553692}}),
+		},
+		{
+			name: "(2x2)の行列のExp",
+			fields: fields{
+				Vec: [][]float32{{1, 2}, {3, 4}},
+			},
+			/*
+				>>> b = np.array([[1,2],[3,4]])
+				>>> np.exp(b)
+				array([[ 2.71828183,  7.3890561 ],
+				       [20.08553692, 54.59815003]])
+			*/
+			want: NewMx([][]float32{{2.71828183, 7.3890561}, {20.08553692, 54.59815003}}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Mx{
+				Vec: tt.fields.Vec,
+				T:   tt.fields.T,
+			}
+			if got := m.Exp(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Mx.Exp() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a Mx
+		w Mx
+	}
+	tests := []struct {
+		name string
+		args args
+		want Mx
+	}{
+		// TODO: Add test cases.
+		{
+			name: "スカラーを加算",
+			args: args{
+				a: NewMx([][]float32{{1, 2, 3}}),
+				w: NewMx([][]float32{{10}}),
+			},
+			want: NewMx([][]float32{{11, 12, 13}}),
+		},
+		{
+			name: "行列を加算",
+			args: args{
+				a: NewMx([][]float32{{1, 2, 3}, {4, 5, 6}}),
+				w: NewMx([][]float32{{10, 10, 10}}),
+			},
+			want: NewMx([][]float32{{11, 12, 13}, {14, 15, 16}}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.a, tt.args.w); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
